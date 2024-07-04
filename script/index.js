ac0d30290f4795de7ecf2725ac252015c4114e5f
Tendry <tendryzephyrin@gmail.com>
2024-07-04 07:04:25 +0200

    Modification du fichier json et du code JS

--- script/index.js
+++ script/index.js
@@ -1,10 +1,20 @@
 import { recupData } from "./data.js"
 
 const search = document.getElementById("search")
-const select = document.getElementById("select")
+const form = document.getElementById('form')
 const tbody = document.querySelector('.tbody')
 
-function appendTo (data)
+form.addEventListener('submit', (e) => {
+     e.preventDefault()
+})
+
+
+/**
+ * Affiche les données récupérées via fetch dans le tableau
+ * @param {Array} data 
+ * @param {HTMLElement} data 
+ */
+function populateTable (data, container)
 {
      data.forEach((element, index) => {
           const tr = document.createElement('tr')
@@ -16,10 +26,49 @@ function appendTo (data)
           tr.appendChild(prenom)
           tr.appendChild(age)
           tr.appendChild(poste)
-          tbody.appendChild(tr)
+          container.appendChild(tr)
      })
 }
 
+
+search.addEventListener('input', async () => {
+     const data = await recupData()
+     let timeOut;
+     clearTimeout(timeOut)
+     timeOut = setTimeout(async () => {
+          if (search.value !== "") {
+               const value = search.value
+               tbody.innerHTML = ''
+               if (Array.isArray(data)) {
+                    append(data, value, tbody)
+               }     
+          }
+          else {
+               populateTable(data, tbody)
+          }
+     }, 100)
+})
+
+
+/**
+ * Filtre les données en fonction de la requête de recherche et met à jour le tableau
+ * @param {Array} data 
+ * @param {string} valeur 
+ * @param {HTMLElement} container 
+ */
+function append (data, valeur, container) 
+{
+     let filterData = []
+     filterData = data.filter(element => element.nom.toLowerCase().includes(valeur.toLowerCase()) || element.prenom.toLowerCase().includes(valeur.toLowerCase()))
+     populateTable(filterData, container) 
+}
+
+/**
+ * Création des td dans le tbody
+ * @param {string} valeur 
+ * @param {Object} options 
+ * @returns HTMLElement
+ */
 function createTd (valeur, options = {})
 {
      const node = document.createElement('td')
@@ -30,11 +79,13 @@ function createTd (valeur, options = {})
      return node
 }
 
-(async function appenData ()
+/**
+ * Affichage initiale des données recupérées dans le tableau
+ */
+(async function appendData ()
 {
      const data = await recupData()
      if (Array.isArray(data)) {
-          appendTo(data)
+          populateTable(data, tbody)
      }
-     else console.log("")
 })()
